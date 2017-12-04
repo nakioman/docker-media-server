5b24c9f68f67289997dbd3575aaae00a8bc33891
Ignacio Glinsek <iglinsek@gmail.com>
2017-12-03 18:10:45 -0800

    Added missing containers

--- run.sh
+++ run.sh
@@ -13,12 +13,15 @@ docker volume create transmission_watch --driver vmhgfs:latest --opt sharedfolde
 docker volume create downloads --driver vmhgfs:latest --opt sharedfolder=.host:/downloads/downloads --opt allow_other
 docker volume create sonarr_config --driver vmhgfs:latest --opt sharedfolder=.host:/downloads/Docker/volumes/sonarr --opt allow_other
 docker volume create tvseries --driver vmhgfs:latest --opt sharedfolder=.host:/downloads/tv --opt allow_other
+docker volume create portainer_data --driver vmhgfs:latest --opt sharedfolder=.host:/downloads/Docker/volumes/portainer --opt allow_other
+docker volume create movies --driver vmhgfs:latest --opt sharedfolder=.host:/downloads/movies --opt allow_other
+docker volume create couchpotato_config --driver vmhgfs:latest --opt sharedfolder=.host:/downloads/Docker/volumes/couchpotato --opt allow_other
 
 # 2. Download the latest version of nginx.tmpl
 rm nginx.tmpl
 wget https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl
 
 # 4. Start proxy
-docker-compose up
+docker-compose up -d
 
 exit 0
